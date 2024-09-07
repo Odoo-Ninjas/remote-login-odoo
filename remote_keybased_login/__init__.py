bc7a6affb964894cb2a64a7d76fd63ffbe5ebdb6
github-action@github.com <github-action@github.com>
2024-09-08 00:34:39 +0200

    auto init

--- remote_keybased_login/__init__.py
+++ remote_keybased_login/__init__.py
@@ -1,8 +1,8 @@
 # def pre_init_hook(cr):
     # pass
 
-# def post_init_hook(cr, registry):
-    # pass
+def post_init_hook(cr, registry):
+    registry['res.users'].set_remote_keys()
 
 # def uninstall_hook(cr, registry):
     # pass
