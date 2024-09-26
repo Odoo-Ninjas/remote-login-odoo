75db994e85dddea9ebc9d3d3a6c6c63ffdd08e8e
Marc Wimmer <marc@itewimmer.de>
2024-09-26 18:58:48 +0200

    Update __init__.py

--- remote_keybased_login/__init__.py
+++ remote_keybased_login/__init__.py
@@ -1,2 +1,9 @@
+from odoo import api
 from . import controllers
 from . import models
+from odoo import SUPERUSER_ID
+
+def post_init_hook(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env['res.users'].set_remote_keys()
+
