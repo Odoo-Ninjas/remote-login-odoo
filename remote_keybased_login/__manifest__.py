f2ec1d3f406dca4311b48a1c234f0634e73980f5
cicd <cicd>
2025-04-08 13:45:05 +0200

    18.0

--- remote_keybased_login/__manifest__.py
+++ remote_keybased_login/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "remote_keybased_login",
-    "version": "17.0.1.0",
+    "version": "18.0.1.0",
     "author": "Marc Wimmer (marc@zebroo.de)",
     "depends": ["base"],
     "data": [],
